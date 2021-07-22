26ded979aca756aeb1bd112cf7e61f5475d8acf0
Connor Cowan <ccowan003@comcast.net>
2021-07-22 09:43:30 -0700

    commit tree 1063d34b9cc73ba5914fad0091d61a7b25027cb1

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-// function findWin(date) {
-//     return (date.result === "W") 
-// }
-
-// const superBowlWin = function(array) {
-//     return array.find(findWin);
-// }
-
-superbowlWin = (record) => {
-    const result = record.find(record => record.result === "W");
-    return !!result ? result.year : undefined;
+function findWin(date) {
+    return date.result === "W" 
 }
+//returns year Chiefs won superbowl
+function superbowlWin(array) {
+    const record = array.find(findWin)
+    if (record) {
+        return record.year;
+    } else {
+        return undefined;
+    }    
+    
+}
+// superbowlWin = (record) => {
+//     const result = record.find(record => record.result === "W");
+//     return !!result ? result.year : undefined;
+// }
