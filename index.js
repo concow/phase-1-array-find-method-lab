b53cacd4462585be21a5e9c07c660c45e94bdabb
Connor Cowan <ccowan003@comcast.net>
2021-07-22 09:09:05 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+// function findWin(date) {
+//     return (date.result === "W") 
+// }
 
+// const superBowlWin = function(array) {
+//     return array.find(findWin);
+// }
+
+superbowlWin = (record) => {
+    const result = record.find(record => record.result === "W");
+    return !!result ? result.year : undefined;
+}
